67240a54a89bb1a7c4188659140fa63bad068fca
Anurag23goel <anurag.go3l07@gmail.com>
2025-03-27 12:25:22 +0530

    Plans for admin added

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,4 +1,10 @@
+import { clientDB } from "../config/db.connection.js";
 import ADMIN from "../models/admin.model.js";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+const db = clientDB.useDb(process.env.CLIENT_DB_NAME);
+const ALL_PLANS = db.collection("plans");
 
 export const ADMIN_REGITSER = async (req, res) => {
   const {
@@ -97,7 +103,7 @@ export const ADMIN_LOGIN = async (req, res) => {
 
 export const GET_ADMIN_DETAILS = async (req, res) => {
   const admin = req.admin;
-  
+
   try {
     if (!admin) {
       return res
@@ -141,5 +147,130 @@ export const ADMIN_LOGOUT = async (req, res) => {
   } catch (error) {
     console.log("Error while logging out user: ", error.message);
     res.status(500).json({ message: "Error while logging out user" });
+  }
+};
+
+export const GET_ALL_PLANS = async (req, res) => {
+  try {
+    const allPlans = await ALL_PLANS.find().toArray();
+    return res.status(200).json({ success: true, allPlans });
+  } catch (error) {
+    console.log("Errors getting all plans for admin - ", error.message);
+    return res
+      .status(500)
+      .json({ success: false, message: "Error while getting all plans" });
+  }
+};
+
+// CREATE PLAN
+export const CREATE_PLAN = async (req, res) => {
+  try {
+    const {
+      name,
+      price,
+      duration,
+      maxProperties,
+      maxActiveProperties,
+      maxSubUsers,
+      features,
+      featuresnotallowed,
+    } = req.body;
+
+    const newPlan = {
+      name,
+      price,
+      duration,
+      maxProperties,
+      maxActiveProperties,
+      maxSubUsers,
+      features: features || [],
+      featuresnotallowed: featuresnotallowed || [],
+      isCustom: false,
+      status: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    const result = await ALL_PLANS.insertOne(newPlan);
+
+    return res.status(201).json({
+      success: true,
+      plan: { _id: result.insertedId, ...newPlan },
+    });
+  } catch (error) {
+    console.log("Internal Server error while creating plan -", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server error while creating plan",
+    });
+  }
+};
+
+// UPDATE PLAN
+export const UPDATE_PLAN = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ success: false, message: "Invalid Plan ID" });
+  }
+
+  const id_for_database_searching = new mongoose.Types.ObjectId(id);
+
+  try {
+    const plan = await ALL_PLANS.findOne({ _id: id_for_database_searching });
+
+    if (!plan) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Plan not found" });
+    }
+
+    const updateData = {
+      ...req.body,
+      updatedAt: new Date(),
+    };
+
+    const result = await ALL_PLANS.findOneAndUpdate(
+      { _id: id_for_database_searching },
+      { $set: updateData },
+      { returnDocument: "after" } // MongoDB native option
+    );
+
+    return res.status(200).json({ success: true, plan: result.value });
+  } catch (error) {
+    console.log("Internal Server error while updating plan -", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server error while updating plan",
+    });
+  }
+};
+
+// DELETE PLAN
+export const DELETE_PLAN = async (req, res) => {
+  const { id } = req.params;
+  const planID_for_database_searching = new mongoose.Types.ObjectId(id);
+
+  try {
+    const result = await ALL_PLANS.deleteOne({
+      _id: planID_for_database_searching,
+    });
+
+    if (result.deletedCount === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Plan not found" });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Plan deleted successfully",
+    });
+  } catch (error) {
+    console.log("Internal Server error while deleting plan -", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server error while deleting plan",
+    });
   }
 };
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -4,15 +4,23 @@ import {
   ADMIN_LOGOUT,
   ADMIN_REGITSER,
   CHECK_ADMIN_AUTH,
+  CREATE_PLAN,
+  DELETE_PLAN,
   GET_ADMIN_DETAILS,
-} from "../controllers/auth.controller.js";
+  GET_ALL_PLANS,
+  UPDATE_PLAN,
+} from "../controllers/admin.controller.js";
 import { VERIFY_ADMIN } from "../middlewares/auth.middleware.js";
 const adminRouter = express.Router();
 
 adminRouter.post("/adminRegister", ADMIN_REGITSER);
 adminRouter.post("/adminLogin", ADMIN_LOGIN);
-adminRouter.get('/get-admin-details', VERIFY_ADMIN, GET_ADMIN_DETAILS)
+adminRouter.get("/get-admin-details", VERIFY_ADMIN, GET_ADMIN_DETAILS);
 // adminRouter.get('/check-admin-auth', CHECK_ADMIN_AUTH)
-adminRouter.post("/adminLogout",VERIFY_ADMIN, ADMIN_LOGOUT);
+adminRouter.post("/adminLogout", VERIFY_ADMIN, ADMIN_LOGOUT);
+adminRouter.get("/get-all-plans", VERIFY_ADMIN, GET_ALL_PLANS);
+adminRouter.post("/create-plan", VERIFY_ADMIN, CREATE_PLAN);
+adminRouter.put("/update-plan/:id", VERIFY_ADMIN, UPDATE_PLAN);
+adminRouter.delete("/delete-plan/:id", VERIFY_ADMIN, DELETE_PLAN);
 
 export default adminRouter;
